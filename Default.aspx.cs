6ddf6a35dce34db01234071a4e62d12bfc78718f
jackHosier <jehosier@asu.edu>
2022-11-30 11:08:00 -0700

    Update Default.aspx.cs
    added button handler

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -11,7 +11,16 @@ namespace PokedexDatabaseProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //this is probably where we need to connect to the DB
 
+        }
+
+        //this is the handler for the serach button 
+        protected void SearchBtn_Click(object sender, EventArgs e)
+        {
+            string query = QueryBox.Text; 
+
+            //add the rest here for searching the database 
         }
     }
 }
