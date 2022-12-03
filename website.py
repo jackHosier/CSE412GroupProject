0181e75a7d0beb847616d43062996d8ab5046d54
jackHosier <jehosier@asu.edu>
2022-12-03 12:04:00 -0700

    Update website.py

--- website.py
+++ website.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,12 +11,26 @@ def get_db_connection():
                             password='3023')
     return conn
 
+#this is for getting text from the textbox 
+@app.route('/')
+def my_form():
+    return render_template('index.html')
 
+@app.route('/', methods =['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
+#for the button 
 @app.route('/')
 def index():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM pokemon;')
+
+    query = my_form_post() #return the string in the text box to this str variable here 
+
+    cur.execute(query)
     poke = cur.fetchall()
     cur.close()
     conn.close()
@@ -31,3 +45,4 @@ def my_link():
     cur.close()
     conn.close()
     return render_template('index.html', poke=poke)
+
